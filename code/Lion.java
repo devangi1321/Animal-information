2c08b00b691a93d251895d14b0daedf93de7e0d0
devangi1321 <devangigurav2007@gmail.com>
2025-08-17 21:01:08 +0530

    Create Lion.java
    Lion.java is a class

---
+++ code/Lion.java
@@ -0,0 +1,7 @@
+package Sounds;
+
+public class Lion extends Sounds{
+    public Lion(String name, String habitat, int legs, boolean tail, boolean trunk, String color, String sound) {
+        super(name, habitat, legs, tail, trunk, color, sound);
+    }
+}
