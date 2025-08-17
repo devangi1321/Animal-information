ebddcdc3df05733841599027aae43e2c737b343f
devangi1321 <devangigurav2007@gmail.com>
2025-08-17 21:01:56 +0530

    Create Elephant.java
    Elephant.java is a class

---
+++ code/Elephant.java
@@ -0,0 +1,7 @@
+package Sounds;
+
+public class Elephant extends Sounds{
+    public Elephant(String name, String habitat, int legs, boolean tail, boolean trunk, String color, String sound) {
+        super(name, habitat, legs, tail, trunk, color, sound);
+    }
+}
