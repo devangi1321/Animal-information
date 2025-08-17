ec46f1345604120497d119689d2b1514ae3a48a0
devangi1321 <devangigurav2007@gmail.com>
2025-08-17 21:02:35 +0530

    Create Duck.java
    Duck.java is a class

---
+++ code/Duck.java
@@ -0,0 +1,7 @@
+package Sounds;
+
+public class Duck extends Sounds{
+    public Duck(String name, String habitat, int legs, boolean tail, boolean trunk, String color, String sound) {
+        super(name, habitat, legs, tail, trunk, color, sound);
+    }
+}
